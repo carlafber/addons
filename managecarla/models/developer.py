18edb0a52ef8d7153c9496ac58cd6a6bc8080711
carlafber <carla.fueber@educa.jcyl.es>
2024-12-19 14:06:38 +0100

    Commir final?

--- managecarla/models/developer.py
+++ managecarla/models/developer.py
@@ -6,14 +6,9 @@ class developer(models.Model):
     _name = 'res.partner'
     _inherit = 'res.partner'
     
-    is_dev = fields.Boolean(string="¿Es Desarrollador?")
+    is_dev = fields.Boolean(string="¿Es Desarrollador?", default=True)
     
     technologies_id = fields.Many2many(string = "Tecnologías", comodel_name = "managecarla.technology", 
                                        relation = "developer_technologies",
                                        column1 = "developer_id", column2 = "technologies")
-    
-    @api.model
-    def create(self, vals):
-        # Si se crea un registro desde este modelo, el campo is_dev se marca como True
-        vals['is_dev'] = True
-        return super(developer, self).create(vals)
+   
